996b410d0c0364854060a79a509195dda7470c36
Mark Mims <markmims@google.com>
2022-03-24 10:51:02 -0600

    working autoscaler

--- terraform/application-mig/main.tf
+++ terraform/application-mig/main.tf
@@ -54,26 +54,13 @@ resource "google_compute_instance_template" "mig_app_server_template" {
 }
 
 resource "google_compute_health_check" "autohealing" {
-  name                = "autohealing-health-check"
-  check_interval_sec  = 5
-  timeout_sec         = 5
-  healthy_threshold   = 2
-  unhealthy_threshold = 10 # 50 seconds
-
-  http_health_check {
-    request_path = "/healthz"
-    port         = "8080"
-  }
-}
-
-resource "google_compute_region_health_check" "autohealing" {
   name        = "autohealing-health-check"
   description = "Health check via tcp"
 
   timeout_sec         = 30
-  check_interval_sec  = 5
-  healthy_threshold   = 2
-  unhealthy_threshold = 10 # 50 seconds
+  check_interval_sec  = 300
+  healthy_threshold   = 1
+  unhealthy_threshold = 10
 
   tcp_health_check {
     port               = "80"
@@ -85,12 +72,28 @@ resource "google_compute_region_instance_group_manager" "regional_mig" {
   base_instance_name = "regional-mig-app-server"
   region               = "us-central1"
   distribution_policy_zones = ["us-central1-c","us-central1-f"]
-  target_size        = local.num_app_servers
+  #target_size        = local.num_app_servers
   version {
     instance_template  = google_compute_instance_template.mig_app_server_template.id
   }
   auto_healing_policies {
-    health_check      = google_compute_region_health_check.autohealing.id
+    health_check      = google_compute_health_check.autohealing.id
     initial_delay_sec = 300
+  }
+}
+
+resource "google_compute_region_autoscaler" "application_autoscaler" {
+  name   = "application-autoscaler"
+  region = var.region
+  target = google_compute_region_instance_group_manager.regional_mig.id
+
+  autoscaling_policy {
+    max_replicas    = local.num_app_servers
+    min_replicas    = 2
+    cooldown_period = 60
+
+    cpu_utilization {
+      target = 0.5
+    }
   }
 }
